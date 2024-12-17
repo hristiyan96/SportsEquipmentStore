37bde0ab614760ca0faf91fb1cbed176db77332b
Hristiyan Stefanov Velev <145477797+hristiyan96@users.noreply.github.com>
2024-12-17 22:15:21 +0200

    Delete SportsEquipmentStore/Models directory

--- SportsEquipmentStore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SportsEquipmentStore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- SportsEquipmentStore/Models/ShoppingCartItem.cs
+++
@@ -1,8 +0,0 @@
-﻿public class ShoppingCartItem
-{
-    public int EquipmentId { get; set; }
-    public string EquipmentName { get; set; }
-    public decimal Price { get; set; }
-    public int Quantity { get; set; }
-    public decimal Total => Price * Quantity;
-}
