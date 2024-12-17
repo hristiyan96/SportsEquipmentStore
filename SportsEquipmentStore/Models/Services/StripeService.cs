9db6dd0b9f7f1f0daf6afcd70d00550b512a1cdb
Hristiyan Stefanov Velev <145477797+hristiyan96@users.noreply.github.com>
2024-12-17 22:16:24 +0200

    Add files via upload

---
+++ SportsEquipmentStore/Models/Services/StripeService.cs
@@ -0,0 +1,27 @@
+﻿using Stripe;
+
+public class StripeService
+{
+    private readonly IConfiguration _configuration;
+
+    public StripeService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+
+        // Replace with your Secret Key from the Stripe Dashboard
+        StripeConfiguration.ApiKey = _configuration["Stripe:SecretKey"];
+    }
+
+    public PaymentIntent CreatePaymentIntent(long amount)
+    {
+        var options = new PaymentIntentCreateOptions
+        {
+            Amount = amount,
+            Currency = "usd",
+            PaymentMethodTypes = new List<string> { "card" }
+        };
+
+        var service = new PaymentIntentService();
+        return service.Create(options);
+    }
+}
